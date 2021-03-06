3e50a215ce919159fb82dfeb1b54b7a570a2f94b
skarab42 <skarab@onlfait.ch>
2021-03-06 11:13:12 +0100

    Ajoutez des fichiers projet.

---
+++ Hudini/GameClasses/Player.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace Hudini
+{
+    /// <summary>
+    /// Add ValheimPlus intro to compendium.
+    /// </summary>
+    [HarmonyPatch(typeof(Player), "OnSpawned")]
+    public static class ModifyOnSpawned
+    {
+        private static void Prefix()
+        {
+            Tutorial.TutorialText introTutorial = new Tutorial.TutorialText()
+            {
+                m_label = "Hudini Test",
+                m_name = "hudini_1",
+                m_text = "Prout prout prout",
+                m_topic = "Welcome to Hudini"
+            };
+
+            if (!Tutorial.instance.m_texts.Contains(introTutorial))
+            {
+                Tutorial.instance.m_texts.Add(introTutorial);
+            }
+
+            Player.m_localPlayer.ShowTutorial("hudini_1");
+        }
+    }
+}
---
+++ Hudini/HudiniPlugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Hudini
+{
+    [BepInPlugin("org.bepinex.plugins.hudini", "Hudini", version)]
+    public class HudiniPlugin : BaseUnityPlugin
+    {
+        public const string version = "0.1.0";
+
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Hello from Hudini!");
+
+            Harmony harmony = new Harmony("mod.hudini");
+            harmony.PatchAll();
+        }
+    }
+}
